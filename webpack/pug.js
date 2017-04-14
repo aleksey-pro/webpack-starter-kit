b8140fbd3a40fa400b20e11bb6fab4617044e123
dave_ghan <anxieter@yandex.ru>
2017-04-14 17:15:28 +0300

    split config

---
+++ webpack/pug.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+  return {
+    module: {
+      rules: [{
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {pretty: true}
+        }
+      ]
+    }
+  }
+};
